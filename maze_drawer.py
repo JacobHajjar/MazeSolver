aced007534d318be5d2337d79219e4d337808a10
Jacob Hajjar <jacob.hajjar1@gmail.com>
2022-04-17 23:18:50 -0700

    some saves

--- maze_drawer.py
+++ maze_drawer.py
@@ -1,5 +1,6 @@
 '''class containing the maze drawing functionality'''
 from collections import namedtuple
+from ctypes import sizeof
 import sys
 import math
 import pygame
@@ -22,12 +23,16 @@ class MazeDrawer:
                     (0,  0, 255), (0, 255,   0), (0, 255, 255), (128,  0, 128))
     curr_mouse = [-1, -1]
     grid = []
+    window_width = 800
+    window_height = 640
+    display_surf = pygame.display.set_mode((window_width, window_height))
 
-    def _init_(self, display_surf):
-        self.display_surf = display_surf
+    def _init_(self):
+        pygame.display.set_caption("Maze Solver")
 
     def start_drawing(self):
         '''start the drawing of the maze'''
+        self.generate_grid(20)  # generate the grid 20 x 15
         fps_clock = pygame.time.Clock()
         while True:  # game started loop
             self.curr_mouse = [-1, -1]
@@ -50,8 +55,18 @@ class MazeDrawer:
 
     def generate_grid(self, dims):
         '''generate the maze grid'''
-        self.grid =  [[BLANK for _ in range(dims)] for _ in range(math.floor(dims * 0.75))]
+        self.grid = [[BLANK for _ in range(dims)]
+                     for _ in range(math.floor(dims * 0.75))]
+        self.grid[7][0] = START
+        self.grid[7][19] = GOAL
 
     def draw_grid(self):
         '''draw the maze grid'''
-       
+        margin = 20
+        box_size = (self.window_width - (margin * 2)) / sizeof(self.grid[0])
+
+    def convert_to_grid(self):
+        '''convert pixel coordinate to grid coordinate'''
+
+    def get_grid_index(self):
+        '''get the grid index from grid coordinate'''
--- maze_solver.py
+++ maze_solver.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-import sys
-import pygame
 '''a maze solving AI using theh A* algorithm'''
+import pygame
+from maze_drawer import MazeDrawer
+pygame.init()
 
 __author__ = 'Jacob Hajjar'
 __email__ = 'hajjarj@csu.fullerton.edu'
 __maintainer__ = 'jacobhajjar'
 
+
 class MazeSolver:
     '''class that will solve the drawn maze'''
-    
+
 
 def main():
     '''the main function'''
-    window_width = 800
-    window_height = 640
-    display_surf = pygame.display.set_mode((window_width, window_height))
-    pygame.display.set_caption("Maze Solver")
-    
+    maze_drawer1 = MazeDrawer()
+    maze_drawer1.start_drawing()
 
-    
 
 if __name__ == '__main__':
     main()
